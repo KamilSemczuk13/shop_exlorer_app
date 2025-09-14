82ea8e064d94955622ba0374ee853ee3a98b2e04
KamilSemczuk13 <szwemcz2005@gmail.com>
2025-09-14 15:14:41 +0200

    changes in app.py, adding etl in pyhton and python fuctions

--- app.py
+++ app.py
@@ -44,6 +44,9 @@ if "clients"not in st.session_state:
 if 'year_client' not in st.session_state:
     st.session_state["year_client"]=""
 
+if "client_tab" not in st.session_state:
+    st.session_state["client_tab"]=""
+
 
 
 #### FUNCTIONS
@@ -145,16 +148,33 @@ def get_years_filter_to_query_sql(year):
         year_having=f"Having TotalSales is not null and OrderYear is not null and OrderYear ='{year}'"
 
     return year_group, year_having
+
+# Function to get choosen performance directly to sql query
+def get_performance_sql(performance):
+    
+    performance_dict={
+        BEST_PERFORMANCE:"desc",
+        WORST_PERFORMANCE:"asc"
+    }
+
+    return performance_dict[performance]
+
+# Function to get choosen performance to python sort
+def get_performance_python(performance):
+    performance_dict={
+        BEST_PERFORMANCE:False,
+        WORST_PERFORMANCE:True
+    }
+
+    return performance_dict[performance]
+
     
 
 # Function to get customers performance from data
 def get_top_perfromance_customers(performance, year, country):
 
      # Filtring per performance
-    if performance == BEST_PERFORMANCE:
-        performance="desc"
-    else: 
-        performance="asc"
+    performance=get_performance_sql(performance)
 
     year_group, year_having=get_years_filter_to_query_sql(year)
 
@@ -188,11 +208,8 @@ def get_top_perfromance_customers(performance, year, country):
 # Function to get products performance from data
 def get_top_perfromance_products(performance, year, country):
 
-     # Filtring per performance
-    if performance == BEST_PERFORMANCE:
-        performance="desc"
-    else: 
-        performance="asc"
+    # Filtring per performance
+    performance=get_performance_sql(performance)
 
     year_group, year_having=get_years_filter_to_query_sql(year)
 
@@ -229,10 +246,7 @@ def get_top_perfromance_products(performance, year, country):
 def get_top_perfromance_categories(performance, year, country):
 
      # Filtring per performance
-    if performance == BEST_PERFORMANCE:
-        performance="desc"
-    else: 
-        performance="asc"
+    performance=get_performance_sql(performance)
 
     year_group, year_having=get_years_filter_to_query_sql(year)
     country=""
@@ -275,10 +289,7 @@ def get_top_perfromance_categories(performance, year, country):
 def get_top_perfromance_moths(performance, year, country):
 
      # Filtring per performance
-    if performance == BEST_PERFORMANCE:
-        performance="desc"
-    else: 
-        performance="asc"
+    performance=get_performance_sql(performance)
 
     year_group, year_having=get_years_filter_to_query_sql(year)
     country=""
@@ -316,10 +327,7 @@ def get_top_perfromance_moths(performance, year, country):
 
 # Description of action of the leaderboard
 def get_description_text_top_mode(performance, tab, type_of_sales, years):
-    # performance_dict={
-    #     BEST_PERFORMANCE:"Performance from the best",
-    #     WORST_PERFORMANCE:"Performance from the worst" 
-    # }
+    
     text=f"The LEADERBOARD below is showing you {performance}  *{tab}* in **{type_of_sales}** in **{years}**"
 
     return text
@@ -399,64 +407,233 @@ def get_categories_of_the_client(year, customer):
     df_category_of_cleint=get_df_from_db(query)
     return df_category_of_cleint
 
-# Get columns from customer table
-def get_columns_of_customers(year, customer):
-    
-    year_group, year_having=get_years_filter_to_query_sql(year)
-    country=""
 
-    query=f'''
+# Geting specyfic data from every tab that is needed
+def get_specyfic_data_from_any_tab(select,where,from_var):
+
+    string_select_query=select
+    
+    string_where_query=where
+    if string_where_query==0:
+        string_where_query=""
+    else:
+        string_where_query=f"Where {string_where_query}"
+
+    with sqlite3.connect("db_shop.db") as conn:
+        query=f''' 
+        
 
         Select
-            *
-        customers
-    '''
+            {string_select_query}
+        From {from_var}
+        {string_where_query}
+        '''
 
-    # Conecting to db
-    df_customers_columns=get_df_from_db(query)
-    df_customers_columns_cleaned=get_cleaned_data(df_customers_columns)
-    return df_customers_columns_cleaned
+    cursor=conn.execute(query)
 
-# Get column from order table
-def get_columns_of_orders(year, customer):
+    data=cursor.fetchall()
+
+    columns=[description[0] for description in cursor.description]
+
+    df=pd.DataFrame(data, columns=columns)
+
+    return df
+
+### Function to transform data with python
+
+# Building string select
+def build_select(select_col):
+    if select_col==0:
+        return 0
+    conditions=[]
+
+    # creating select string sql injection
+    for col in select_col:
+        if col==0:
+            continue
+        conditions.append(f"{col}")
+        
+    # returning select string sql injection
+    return " , ".join(conditions)
+
+# building string where
+def build_where(where_col: dict):
+    conditions = []
+    for col, val in where_col.items():
+        if val in [0, ALL_YEARS]:
+            continue  # are not paying attention on 0 values
+        if isinstance(val, str):
+            conditions.append(f"{col}='{val}'")
+        else:
+            conditions.append(f"{col}={val}")
+
+    if not conditions:  # if every of the value is 0
+        return 0   # retrun 0 if every value is 0
+
+    return " AND ".join(conditions)
+
+# Function to cleaning data from nulls and fake nulls
+def cleaning_data(df, col):
+
+    return df[
+        (df[col].notna()) & (df[col].astype(str).str.strip() != "") &
+        (df[col].astype(str).str.strip().str.lower() != "null") & 
+        (df[col].astype(str).str.strip().str.lower() != "none")
+    ]
+
+# Geting cleaned df
+def get_cleaned_data(df):
+
+    for col in df.columns:
+        df=cleaning_data(df, col)
+
+    return df
+
+# Function to get dict of querries to extract data from db
+def creating_dict_to_sql_query(list_column_of_tabs, filters_list_dict):
+
+    # get secelct string for each tab
+    customers_select=build_select(list_column_of_tabs[0])
+    orders_select=build_select(list_column_of_tabs[1])
+    orderdetails_select=build_select(list_column_of_tabs[2])
+    products_select=build_select(list_column_of_tabs[3])
+
+    # get where string dor each tab
+    customers_where=build_where(filters_list_dict[0])
+    orders_where=build_where(filters_list_dict[1])
+    orderdetails_where=build_where(filters_list_dict[2])
+    products_where=build_where(filters_list_dict[3])
+
+    # Dictionary of selects and where to each tab
+    dict_of_cols_tabs={
+        
+        "customers":{
+            "select":customers_select,
+            "where":customers_where
+        },
+        "orders":{
+            "select":orders_select,
+            "where":orders_where
+        },
+        "orderdetails":{
+            "select":orderdetails_select,
+            "where":orderdetails_where
+        },
+        "products":{
+            "select":products_select,
+            "where":products_where
+        },
+    }
+
+    return dict_of_cols_tabs
     
-    year_group, year_having=get_years_filter_to_query_sql(year)
-    country=""
+# Get all tabs that are nedeed to analyse
+def get_all_data_from_tables(dict_of_cols_tbas):
 
-    query=f'''
+    # Taking columns from specyfic tab to create sql select query or 0 if not using
+    df_customers_ret=dict_of_cols_tbas["customers"]["select"]
+    df_orders_ret=dict_of_cols_tbas["orders"]["select"]
+    df_orderdetails_ret=dict_of_cols_tbas["orderdetails"]["select"]
+    df_products_ret=dict_of_cols_tbas["products"]["select"]
 
-        Select
-            *
-        orders
-    '''
+    customers_where=dict_of_cols_tbas["customers"]["where"]
+    orders_where=dict_of_cols_tbas["orders"]["where"]
+    orderdetails_where=dict_of_cols_tbas["orderdetails"]["where"]
+    products_where=dict_of_cols_tbas["products"]["select"]
 
-    # Conecting to db
-    df_orders_columns=get_df_from_db(query)
-    df_orders_columns_cleaned=get_cleaned_data(df_orders_columns)
-    return df_orders_columns_cleaned
+    # variables to take name of the columns to from in select query
+    cust_from_var="Customers"
+    or_from_var="Orders"
+    od_from_var="Orderdetails"
+    pr_from_var="Products"
 
+    # 0 if we dont need table to extract data from
+    if df_customers_ret !=0:
 
-def get_columns_of_orderdetails(year, customer):
+        df_customers=get_specyfic_data_from_any_tab(df_customers_ret,customers_where,cust_from_var)
+        df_customers_ret=get_cleaned_data(df_customers)
+
+    if df_orders_ret !=0:
+
+        df_orders=get_specyfic_data_from_any_tab(df_orders_ret,orders_where,or_from_var)
+        df_orders_ret=get_cleaned_data(df_orders)
+
+    if df_orderdetails_ret !=0:
+
+        df_orderdetails=get_specyfic_data_from_any_tab(df_orderdetails_ret,orderdetails_where,od_from_var)
+        df_orderdetails_ret=get_cleaned_data(df_orderdetails)
+
+    if df_products_ret !=0:
+
+        df_products=get_specyfic_data_from_any_tab(df_products_ret,products_where,pr_from_var)
+        df_products_ret=get_cleaned_data(df_products)
+
+    # dict of data from tables to return variables
+    tabs_dict={
+        "customers":df_customers_ret,
+        "orders":df_orders_ret,
+        "orderdetails":df_orderdetails_ret,
+        "products":df_products_ret
+    }
     
-    year_group, year_having=get_years_filter_to_query_sql(year)
-    country=""
+    return tabs_dict
 
-    query=f'''
+def get_customers_select(select_list):
+    return select_list
 
-        Select
-            *
-        orderdetails
-    '''
+def get_orders_select(select_list):
+    return select_list
 
-    # Conecting to db
-    df_orders_columns=get_df_from_db(query)
-    df_orders_columns_cleaned=get_cleaned_data(df_orders_columns)
-    return df_orders_columns_cleaned
+def get_orderdetails_select(select_list):
+    return select_list
 
+def get_products_select(select_list):
+    return select_list
 
+# Function to get mapped countries column
+def get_mapped_countries_df(df):
+    countries_dict={
+        "polska":"Poland", "pl":"Poland", "poland":"Poland", "polonia":"Poland",
+        "usa":"Usa", "unitedstates":"Usa",
+        "niemcy":"Germany","germany":"Germany", "de":"Germany",
+        "france":"France","fr":"France","francja":"France",
+        "spain":"Spain", "espana":"Spain", "esp":"Spain", "hiszpania":"Spain","españa":"Spain",
+        "china":"China", "Chiny":"China"
+        }
+
+    df["Country_mapped"]=df["Country"].astype(str).str.strip().str.lower().map(countries_dict)
+
+    return df
+# get Top performance countries
+def get_merged_top_performance_country(dict_of_tables, performance):
+
+    performance=get_performance_python(performance)
+    # c-> customers
+    # o-> orders
+    # od-> orderdetails
+    # p -> products
+
+    # Geting data from every tab nedeed
+    df_c_cl=dict_of_tables["customers"]
+    df_o_cl=dict_of_tables["orders"]
+    df_od_cl=dict_of_tables["orderdetails"]
+
+    df_c_cl=get_mapped_countries_df(df_c_cl)
+        
+    df_c_o=df_o_cl.merge(df_c_cl, how="left",on="CustomerID")
+    df_c_o_od=df_c_o.merge(df_od_cl, how="inner", on="OrderID")
+    df_c_o_od["Total_Sale"]=df_c_o_od["Quantity"]*df_c_o_od["UnitPrice"]
+    df_final_merged_ranked=df_c_o_od[["Country_mapped","Total_Sale" ]]
+    df_grouped_country_sales=(df_final_merged_ranked.groupby("Country_mapped").sum()
+                          ).reset_index()
+    df_grouped_country_sales=df_grouped_country_sales.sort_values("Total_Sale", ascending=performance)
+    df_grouped_country_sales["RankedSales"]=df_grouped_country_sales["Total_Sale"].rank(ascending=performance)
+
+    return df_grouped_country_sales
 
 ### FUNCTIONS to visualise data
 
+# Visualise leaderboard
 def leaderboard_html_inline(df, name_col, score_col, rank_col, performance):
     df = df.sort_values(rank_col, ascending=True).reset_index(drop=True)
     items = []
@@ -501,7 +678,13 @@ def leaderboard_html_inline(df, name_col, score_col, rank_col, performance):
     return "\n".join(items)
 
 # Get monthly total sales chart
-def get_total_monthy_sales_chart(df, customer):
+def get_monthly_sales_chart(df, type_of_sales, customer):
+
+    sales_desc={
+        "TotalSales":"Total Sales",
+        "AvgSales": "Average Sales"
+    }
+
     sns.set_style("whitegrid")  
     sns.set_palette("tab10")    
 
@@ -509,7 +692,7 @@ def get_total_monthy_sales_chart(df, customer):
     fig, ax = plt.subplots(figsize=(10,5))
     sns.lineplot(
         x="Month_normal",
-        y="TotalSales",
+        y=f"{type_of_sales}",
         hue="OrderYear",
         data=df,
         marker="o",   
@@ -517,37 +700,9 @@ def get_total_monthy_sales_chart(df, customer):
     )
 
     # Tittle and series osi
-    ax.set_title(f"Monthly Total Sales of {customer}", fontsize=16)
+    ax.set_title(f"Monthly {sales_desc[type_of_sales]} of {customer}", fontsize=16)
     ax.set_xlabel("Month", fontsize=12)
-    ax.set_ylabel("Total Sales", fontsize=12)
-
-    # adding legend
-    ax.legend(title="Year", title_fontsize=12, fontsize=10, loc='upper left')
-
-    return fig
-
-# Get average monthly sales chart
-def get_average_monthy_sales_chart(df, customer):
-
-    # Seaborn style
-    sns.set_style("whitegrid")  
-    sns.set_palette("tab10")    
-
-    # creating plot
-    fig, ax = plt.subplots(figsize=(10,5))
-    sns.lineplot(
-        x="Month_normal",
-        y="AvgSales",
-        hue="OrderYear",
-        data=df,
-        marker="o",    
-        ax=ax
-    )
-
-    # Tittle and series osi
-    ax.set_title(f"Monthly Average Sales of {customer}", fontsize=16)
-    ax.set_xlabel("Month", fontsize=12)
-    ax.set_ylabel("Total Sales", fontsize=12)
+    ax.set_ylabel(f"{sales_desc[type_of_sales]}", fontsize=12)
 
     # adding legend
     ax.legend(title="Year", title_fontsize=12, fontsize=10, loc='upper left')
@@ -562,7 +717,7 @@ def display_charts_monthly_sales_per_customer(df, customer):
     # Seaborn style
     
     # Get total sales chart
-    fig=get_total_monthy_sales_chart(df,customer)
+    fig=get_monthly_sales_chart(df, "TotalSales",customer)
 
     # Displaying in Streamlit 
     st.pyplot(fig)
@@ -571,7 +726,7 @@ def display_charts_monthly_sales_per_customer(df, customer):
     st.info(f"The chart showing monthly Average Sales of {customer} 🔻")
 
     # Get average sales 
-    fig=get_average_monthy_sales_chart(df,customer)
+    fig=get_monthly_sales_chart(df,"AvgSales",customer)
 
     # Displaying in Streamlit 
     st.pyplot(fig)
@@ -582,7 +737,7 @@ def display_charts_monthly_sales_per_customer(df, customer):
 ##
 
 if st.session_state["page"]=="start":
-    st.header("Welcome to the app Shop Data Explorer📊🔍 ")
+    st.header("Welcome to the app Shop Data Explorer ")
     st.markdown("In the app you will analyse data that was created for the shop", unsafe_allow_html=True)
 
     st.info("**If you want to check the app click button below *Continue***")
@@ -600,7 +755,7 @@ if st.session_state["page"]=="main":
         st.info("Click tabs below to view sales performance by customer, product, and more 👇")
 
         #Customers Tab
-        tab1,tab2,tab3,tab4=st.tabs(["Top Customers", "Top Products", "Top Categories", "Top Months"])
+        tab1,tab2,tab3,tab4,tab5=st.tabs(["Top Customers", "Top Products", "Top Categories", "Top Months","Top Countries"])
 
         # Sidebar options
         with st.sidebar:
@@ -621,6 +776,7 @@ if st.session_state["page"]=="main":
 
             description=get_description_text_top_mode(performance, "Customers", "Total Sales", year)
             st.info(description)
+
             # Get df of the customers performance
             df_customers_top = get_top_perfromance_customers(performance, year, "")
             leaderboard_customers = leaderboard_html_inline(df_customers_top, "FullName","TotalSales","RankedClientsBySales",performance)
@@ -656,6 +812,44 @@ if st.session_state["page"]=="main":
             leaderboard_months = leaderboard_html_inline(df_months_top,"Month_Maped", "TotalSales", "RankedMonthsBySales", performance)
             st.markdown(leaderboard_months, unsafe_allow_html=True)
 
+        with tab5:
+            description=get_description_text_top_mode(performance, "Countries", "Total Sales", year)
+            st.info(description)
+
+            customers_select=get_customers_select(["CustomerID","Country"])
+            orders_select=get_orders_select(["CustomerID","OrderDate","OrderID","strftime('%Y', OrderDate) AS Year"])
+            orderdetails_select=get_orderdetails_select(["OrderID","Quantity","UnitPrice"])
+            products_select=get_products_select(0)
+
+            customers_filter={
+                "Customer":0,
+            }
+            orders_filters={
+                "Year":year,
+                "Month":0
+            }
+            orderdetails_filters={
+                "Year":0,
+                "Month":0
+            }
+            products_filters={
+                "Product":0
+            }
+
+            filters=[customers_filter,orders_filters,orderdetails_filters,products_filters]
+
+            list_column_of_tabs=[customers_select,orders_select,orderdetails_select,products_select]
+
+            dict_select_where=creating_dict_to_sql_query(list_column_of_tabs,filters)
+
+            # Dictionary of dataframes with specyfic tab data
+            dict_df_of_tabs=get_all_data_from_tables(dict_select_where)
+
+            df_merged_top_countries=get_merged_top_performance_country(dict_df_of_tabs,performance)
+
+            # Displaying leaderboard of countries
+            leaderboard_countries=leaderboard_html_inline(df_merged_top_countries,"Country_mapped","Total_Sale", "RankedSales", performance)
+            st.markdown(leaderboard_countries, unsafe_allow_html=True)
     # Mode client
     if st.session_state["mode"]==MODE_CLIENT:
 
